9850747e36d0b766a5fa45c921eac945210221b2
Vlad Mosoiu <vlad.m@agorarobotics.com>
2024-05-30 15:25:33 +0300

    Remove print in stream.py

--- bt_live/src/bt_live/stream.py
+++ bt_live/src/bt_live/stream.py
@@ -9,7 +9,7 @@ def response_generator(node):
     while True:
         data = node.spin_once()
         if data:
-            print(data)
+            # print(data)
             yield json.dumps(data)
         yield '\n'
 
